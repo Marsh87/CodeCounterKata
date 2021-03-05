3b716b4131bc6178dc93371b5fe7034496d15d41
Mahesh Moodley <Mahesh.Moodley@chillisoft.co.za>
2021-03-05 16:01:21 +0200

    Add solution to git

---
+++ CodeCounter/CodeCounter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace CodeCounter
+{
+    public class CodeCounter
+    {
+        private  bool _multiLineCommentCheck = false;
+        public int CountLines(string filePath)
+        {
+            var countLines = 0;
+            var multilineCounter = 0;
+            using (var reader = new StreamReader(filePath))
+            {
+                while (reader.Peek() > -1)
+                {
+                    var line = reader.ReadLine();
+                    if (IsLineAComment(line))
+                    {
+                        countLines--;
+                    }
+                    multilineCounter = CountMultiLineComment(line, multilineCounter);
+                    countLines++;
+                }
+            }
+            return countLines - multilineCounter;
+        }
+
+        private  int CountMultiLineComment(string line, int multilineCounter)
+        {
+            if (IsMultiLineCommentStart(line))
+            {
+                _multiLineCommentCheck = true;
+            }
+
+            if (_multiLineCommentCheck)
+            {
+                multilineCounter++;
+            }
+
+            if (IsMultiLineCommentEnd(line))
+            {
+                _multiLineCommentCheck = false;
+            }
+
+            return multilineCounter;
+        }
+
+        private static bool IsMultiLineCommentEnd(string line)
+        {
+            return line != null && line.EndsWith("*/");
+        }
+
+        private static bool IsMultiLineCommentStart(string line)
+        {
+            return line != null && line.StartsWith("/*");
+        }
+
+        private static bool IsLineAComment(string line)
+        {
+            return line != null && line.StartsWith("//");
+        }
+    }
+}
---
+++ CodeCounterTests/CodeCounterTests.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+
+namespace CodeCounterTests
+{
+    [TestFixture]
+    public class CodeCounterTests
+    {
+        [Test]
+        public void CountLines_GivenFileWithOneLine_ShouldReturnOne()
+        {
+            //Arrange 
+            var filePath = @"HasOneLineOfText.txt";
+            var sut = new CodeCounter.CodeCounter();
+            //Act
+            var result = sut.CountLines(filePath);
+            //Assert
+            var expected = 1;
+            Assert.AreEqual(expected,result);
+        } 
+        
+        [Test]
+        public void CountLines_GivenFileWithOneLineOneEmptyLine_ShouldReturnOne()
+        {
+            //Arrange 
+            var filePath = @"HasOneLineOfTextWithEmptyLine.txt";
+            var sut = new CodeCounter.CodeCounter();
+            //Act
+            var result = sut.CountLines(filePath);
+            //Assert
+            var expected = 1;
+            Assert.AreEqual(expected,result);
+        }
+        
+        [Test]
+        public void CountLines_GivenFileWithNoLines_ShouldReturnZero()
+        {
+            //Arrange 
+            var filePath = @"HasNoLines.txt";
+            var sut = new CodeCounter.CodeCounter();
+            //Act
+            var result = sut.CountLines(filePath);
+            //Assert
+            var expected = 0;
+            Assert.AreEqual(expected,result);
+        }
+        
+        [Test]
+        public void CountLines_GivenFileWithOneLineAndSingleLineComment_ShouldReturnOne()
+        {
+            //Arrange 
+            var filePath = @"HasSingleLineComment.txt";
+            var sut = new CodeCounter.CodeCounter();
+            //Act
+            var result = sut.CountLines(filePath);
+            //Assert
+            var expected = 1;
+            Assert.AreEqual(expected,result);
+        } 
+        
+        [Test]
+        public void CountLines_GivenFileWithOneLineAndMultipleSingleLineComment_ShouldReturnTwo()
+        {
+            //Arrange 
+            var filePath = @"HasMultipleSingleLineComment.txt";
+            var sut = new CodeCounter.CodeCounter();
+            //Act
+            var result = sut.CountLines(filePath);
+            //Assert
+            var expected = 2;
+            Assert.AreEqual(expected,result);
+        }
+        
+        [Test]
+        public void CountLines_GivenFileWithOneLineAndMultiLineComment_ShouldReturnTwo()
+        {
+            //Arrange 
+            var filePath = @"HasMultipleLineComment.txt";
+            var sut = new CodeCounter.CodeCounter();
+            //Act
+            var result = sut.CountLines(filePath);
+            //Assert
+            var expected = 2;
+            Assert.AreEqual(expected,result);
+        }
+    }
+}
